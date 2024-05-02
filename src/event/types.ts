d744d8815a0163bae44fb517bb547de90e364288
Ciaran <cslemon96@gmail.com>
2024-05-02 02:06:05 +0200

    wip(src/example/example-react-app): More work on implementation

--- src/event/types.ts
+++ src/event/types.ts
@@ -4,10 +4,10 @@ import type { EventFilters } from ".";
 export type Event = z.infer<typeof Event>
 export const Event = z.object({
     id: z.string().uuid(),
+    position: z.number(),
     date: z.number(),
     type: z.string(),
     data: z.any(),
-    position: z.number(),
 }).transform((msg) => {
     return {
         ...msg,
@@ -19,7 +19,7 @@ export type TypedEvent<T> = Event & { data: T };
 export interface IEventLog {
     log: Event[]
     position: number
-    create<T>(type: string, data: T, position: number): Event & { data: T }
+    create<T>(type: string, data: T, position?: number): Event & { data: T }
     push(event: Event): void
     pos(): number
     project<T>(filters?: EventFilters): Array<TypedEvent<T>>
